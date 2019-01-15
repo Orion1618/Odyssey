81ee3091e5102d30f46af669748ab2f9696bb8aa
Orion1618 <35470964+Orion1618@users.noreply.github.com>
2019-01-14 20:08:28 -0500

    Update .1_Analysis_OutlierDetection.R
    update to v2.0.1

--- SubModules/PopStratModule/.1_Analysis_OutlierDetection.R
+++ SubModules/PopStratModule/.1_Analysis_OutlierDetection.R
@@ -23,19 +23,34 @@
   library(tidyr)
   library(ggplot2)
   library(tidyverse)
+  library(plotly)
+  library(RColorBrewer)
 
 #Set Working Directory
-	#(MANUAL) setwd('/N/dc2/scratch/ryeller/Odyssey/PopStratModule/PCA_Analyses/HGDP-Euro_PCA')
+	#(MANUAL) setwd('./Odyssey/PopStratModule/PCA_Analyses/HGDP-Euro_PCA')
 
 # Read in the Command arguments into the R environment  
   commandArgs()->args
+  
+  args[6]->PCA_Output
+  as.character(PCA_Output)->PCA_Output
+    #(Manual -- Set PCA Output Directory) '/N/dc2/scratch/ryeller/Odyssey/SubModules/PopStratModule/PCA_Analyses/PlotlyAddin' ->PCA_Output
+  
+  args[7]->PC_VariancePerc
+  as.numeric(PC_VariancePerc)-> PC_VariancePerc
+	#(Manual -- Set PC Variance Criteria) 0.01 ->PC_VariancePerc
+  
+  args[8]->PC_StdDev
+   as.numeric(PC_StdDev)-> PC_StdDev
+	#(Manual -- Set PCA Output Directory) 4->PC_StdDev
+	
 
 # --------- Load Files -------------
 
 # Load Eigenvector File
   #(MANUAL) Eigenvector <- fread(file.choose(), sep='auto', data.table = T)
   #(MANUAL2) system(paste0("ls ", getwd(), "/*.eigenvec"), intern=TRUE)->EigenvectorFile
-  system(paste0("ls ", args[6], "/*.eigenvec"), intern=TRUE)->EigenvectorFile
+  system(paste0("ls ", PCA_Output, "/*.eigenvec"), intern=TRUE)->EigenvectorFile
   Eigenvector <- fread(EigenvectorFile, sep='auto', data.table = T)
 
 
@@ -43,7 +58,7 @@
 # Load Eigenvalue File
   #(MANUAL) Eigenvalue <- fread(file.choose(), sep='auto', data.table = T)
   #(MANUAL2) system(paste0("ls ", getwd(), "/*.eigenval"), intern=TRUE)->EigenvalueFile
-  system(paste0("ls ", args[6], "/*.eigenval"), intern=TRUE)->EigenvalueFile
+  system(paste0("ls ", PCA_Output, "/*.eigenval"), intern=TRUE)->EigenvalueFile
   Eigenvalue <- fread(EigenvalueFile, sep='auto', data.table = T)
 
 
@@ -63,7 +78,7 @@
   message('--------------------------------')
   message(' ')
 
-  (Concat.Eigenvalue = Eigenvalue[Eigenvalue$PercentVar > as.numeric(args[7]), ])
+  (Concat.Eigenvalue = Eigenvalue[Eigenvalue$PercentVar > as.numeric(PC_VariancePerc), ])
   #(MANUAL - set Percent Variance to Keep - default == 0.01)(Concat.Eigenvalue = Eigenvalue[Eigenvalue$PercentVar > 0.01, ])
   message(' ')
 
@@ -97,8 +112,8 @@
 # Load the Euro Sample List from the PopStratHelper Excel Doc  
   #(MANUAL) CentroidSamples <- fread(file.choose(), sep='auto', header = F)
   #(MANUAL2) system(paste0("ls ", getwd(), "/*.csv"), intern=TRUE)->CentroidSamplesList
-  system(paste0("ls ", args[6], "/*.csv"), intern=TRUE)->CentroidSamplesList
-  CentroidSamples <- fread(CentroidSamplesList, sep='auto', data.table = T)
+  system(paste0("ls ", PCA_Output, "/*.csv"), intern=TRUE)->CentroidSamplesList
+  CentroidSamples <- fread(CentroidSamplesList, sep='auto', data.table = T, header=F)
 
   names(CentroidSamples)<-c('FID', 'IID')
 
@@ -143,7 +158,7 @@
   message('Max Centroid Cutoff')
   message('--------------------------------')
 
-  (CentroidAVE+as.numeric(args[8])*CentroidSTDEV->MaxCutoff  )
+  (CentroidAVE+(as.numeric(PC_StdDev)*CentroidSTDEV)->MaxCutoff  )
   #(MANUAL -- set STDEV -- default == 3) (CentroidAVE+3*CentroidSTDEV->MaxCutoff  )
 
   message(' ')
@@ -152,14 +167,15 @@
   message('Min Centroid Cutoff')
   message('--------------------------------')
 
-  (CentroidAVE-as.numeric(args[8])*CentroidSTDEV->MinCutoff  )
+  (CentroidAVE-as.numeric(PC_StdDev)*CentroidSTDEV->MinCutoff  )
   #(MANUAL -- set STDEV -- default == 3) (CentroidAVE-3*CentroidSTDEV->MinCutoff  )
 
   message('')
   message(' ')
 
 
-# ----------- Removes the Outliers ------------
+# -------------------- Removes the Outliers -------------------
+
 # Or Remove Individual EigenVectors that lie outside of the User-Specified Centroid
   
 #Set DataTable to DataFrame
@@ -178,7 +194,7 @@
 
 
 
-# --------- Start Plotting -------------
+# ----------------------- Start Plotting ----------------------------
 
 # Remove Referenece People  from Outliers Removed to Get just those for analysis
   JustAnalysis<- anti_join(OutliersRemoved, CentroidSamples, by= c('FID', 'IID'))
@@ -195,6 +211,7 @@
 
 
 # Output plot as PNG
+# -------------------
 
   message('')
   message(' ')
@@ -202,7 +219,7 @@
   message('--------------------------------')
 
   options(bitmapType='cairo') 
-  png(paste0(args[6], '/', 'PCA_Plot.png'))
+  png(paste0(PCA_Output, '/', 'PCA_Plot.png'))
   #(MANUAL -- plots in current working directory) png(paste0(getwd(), '/', 'PCA_Plot.png'))
 
   
@@ -214,11 +231,45 @@
   dev.off()
 
 # Output outlying samples to exclude as well as entire Ref-Target Eigenvector Table:
-  write.table(Outliers[ ,1:2], file=paste0(args[6],'/', 'AncestryOutliers.txt'), row.names=F, col.names = F, sep = "\t", quote = F )
-  write.table(PlotMe, file=paste0(args[6],'/', 'PCA_CompleteTable.txt'), row.names=F, col.names = T, sep = "\t", quote = F )
+  write.table(Outliers[ ,1:2], file=paste0(PCA_Output,'/', 'AncestryOutliers.txt'), row.names=F, col.names = F, sep = "\t", quote = F )
+  write.table(PlotMe, file=paste0(PCA_Output,'/', 'PCA_CompleteTable.txt'), row.names=F, col.names = T, sep = "\t", quote = F )
 
   #(MANUAL -- writes to current working directory) write.table(Outliers[ ,1:2], file=paste0(getwd(),'/', 'AncestryOutliers.txt'), row.names=F, col.names = F, sep = "\t", quote = F )
   #(MANUAL -- writes to current working directory) write.table(PlotMe, file=paste0(getwd(),'/', 'PCA_CompleteTable.txt'), row.names=F, col.names = T, sep = "\t", quote = F )
+  
+  
+# Output plot as Interactive Plotly
+# -----------------------------------
+
+#Load in the eigenvector file (should contain headers -- e.g. PC1, PC2, PC3, and Grouping variable as Group)
+	#(MANUAL -- Load eigenvector + grouping file)  Plotme <- fread(file.choose(), sep='auto', data.table = T, header=T)
+
+# Make pretty colors groups
+	n <- 60
+	qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
+	col_vector = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+
+
+# Make the ploty 3D scatterplot (using PC1=X, PC2=Y, PC3=Z, using the most destinctive color categories -- up to 60 color groups)
+
+p <- plot_ly(PlotMe, x = ~PC1, y = ~PC2, z = ~PC3, color = ~Group, colors = sample(col_vector, 60), marker = list(size=5),
+ text = ~paste0('FID:IID', FID, ":", IID)) %>%
+  add_markers() %>%
+  layout(scene = list(xaxis = list(title = 'PC1'),
+                      yaxis = list(title = 'PC2'),
+                      zaxis = list(title = 'PC3')))
+
+# visualize the Plot locally on R
+	#p
+
+# Save Plot to current working directory
+# Plotely Dependencies will be plotted in the Directory where plot is saved
+	htmlwidgets::saveWidget(p, paste0(PCA_Output, '/PCA_Plot.html'), libdir = paste0(PCA_Output, '/1PlotlyDependencies/'), selfcontained=FALSE) 
+  
+  
+  
+  
+  
 
   message('')
   message(' ')
